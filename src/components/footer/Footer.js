e817aa295fd436171fd6a3e60155e8511014404d
Karthik <karthik5112001@gmail.com>
2023-03-08 14:41:58 +0530

    commit

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+//-----css----
+import './footer.css'
+
+function Footer() {
+    return (
+        <div className='footerHolder'>
+            <div className='footerHolderContainer'>
+                <div className='footerContainer'>
+                    <div>   Address : D125/E, Santhosh Colony, K.K Nagar, Chennai - 60078</div>
+                    <div>  Phone No: 9840717144</div>
+                </div>
+                <div>
+                    <p>All rights are reserved</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Footer
